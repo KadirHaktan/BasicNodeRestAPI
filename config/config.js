984febef55b8ae973dfa2b34224672132382fe2a
Hacktin <isues41@gmail.com>
2020-06-02 15:24:03 +0300

    git ignore has been edited

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 
 const configurationInfos={
-    JWT_SECRET_KEY:"sitesitesitebasicProjectConfig4532",
-    JWT_EXPIRE:3600,
-    JWT_COOKIE_EXPIRE:30,
-    host:"localhost",
-    user:"root",
+    JWT_SECRET_KEY="",
+    JWT_EXPIRE=0,
+    JWT_COOKIE_EXPIRE=0,
+    host:"",
+    user:"",
     password:"",
-    database:"clean-node-api"
+    database:""
 }
 
 module.exports=configurationInfos
